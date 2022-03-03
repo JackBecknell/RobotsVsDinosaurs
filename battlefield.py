e2264f135caebf8328bbfa9e8950c65501173676
JackBecknell <52moosefit@gmail.com>
2022-03-03 18:59:07 -0500

    On the Battlefied file I imported two lists, updated the __init__ to include a couple function calls, added a 'coin-toss' to the run_game function allowing a fair way to let one side go first. Updated the display_welcome function to give the user a nice warm welcome when they start up a game. Finished tweaking the dino_turn and robo_turn so they will continue back and forth until the game gets called. I also added a check_health function that runs after each turn which looks to see if any robots or dinosaurs reached 0 health or lower.

--- battlefield.py
+++ battlefield.py
@@ -1,35 +1,88 @@
 from random import randint
 from fleet import Fleet
+from fleet import names_of_robots
 from herd import Herd
+from herd import name_of_dinosaurs
 
 class Battlefield:
     def __init__(self):
+        self.display_welcome()
         self.fleet_on_battlefield = Fleet()
         self.herd_on_battlefield = Herd()
+        self.run_game()
 
     def run_game(self):
-        Fleet.create_fleet()
-        Herd.create_herd()
-        coin_toss = randint(0,2)
+        self.coin_toss = randint(1,2)
+        if self.coin_toss == 1:
+            self.coin_toss = 'Robots!'
+        elif self.coin_toss == 2:
+            self.coin_toss = 'Dinosaurs!'
+        print(f'And after a quick coin toss it looks like the {self.coin_toss} will take the first turn!')
+        if self.coin_toss == 'Robots!':
+            self.robo_turn()
+        else:
+            self.dino_turn()
 
     def display_welcome(self):
-        pass
+        print(f"Welcome Player! Quite the matchup Dinosaurs and Robots.\n\nOn our Robots side we have {names_of_robots[0]}!  {names_of_robots[1]}! And {names_of_robots[2]}!\n\nAnd in the other corner we have the {name_of_dinosaurs[0]}!, the {name_of_dinosaurs[1]}!, and {name_of_dinosaurs[2]}!\n\nNow for the rules! Each Robot will choose 1 weapon to use all the way through the match.\nWhile the Dinosaurs can switch it up each time.\n\nLet's get started by giving our robots some weapons!")
 
     def battle(self):
         pass
 
     def dino_turn(self):
-        pass
+        self.show_dino_attack_options()
+        self.dino_turn_over = False
+        while self.dino_turn_over != True:
+            self.user_dino_attack_option = input("Enter here:")
+            if self.user_dino_attack_option == 1 or self.user_dino_attack_option == '1':
+                self.fleet_on_battlefield.robots_in_fleet[0].robo_health += self.herd_on_battlefield.dinosaurs_in_herd[0].attack_chosen[1] 
+                self.dino_turn_over = True
+            elif self.user_dino_attack_option == 2 or self.user_dino_attack_option == '2':
+                self.fleet_on_battlefield.robots_in_fleet[0].robo_health += self.herd_on_battlefield.dinosaurs_in_herd[1].attack_chosen[1]
+                self.dino_turn_over = True
+            elif self.user_dino_attack_option == 3 or self.user_dino_attack_option == '3':
+                self.fleet_on_battlefield.robots_in_fleet[0].robo_health += self.herd_on_battlefield.dinosaurs_in_herd[2].attack_chosen[1]
+                self.dino_turn_over = True
+            else:
+                self.user_dino_attack_option = input("Invalid command. Try again:")
+        self.check_health()
+        self.robo_turn()    
+
 
     def robo_turn(self):
-        pass
+        self.show_robo_attack_options()
+        self.robo_turn_over = False
+        while self.robo_turn_over != True:
+            self.user_robo_attack_option = input("Enter here:")  
+            if self.user_robo_attack_option == 1 or self.user_robo_attack_option =='1':
+                self.herd_on_battlefield.dinosaurs_in_herd[0].dino_health += self.fleet_on_battlefield.robots_in_fleet[0].weapon_chosen[1]
+                self.robo_turn_over = True
+            elif self.user_robo_attack_option == 2 or self.user_robo_attack_option =='2':
+                self.herd_on_battlefield.dinosaurs_in_herd[0].dino_health += self.fleet_on_battlefield.robots_in_fleet[1].weapon_chosen[1]
+                self.robo_turn_over = True
+            elif self.user_robo_attack_option == 3 or self.user_robo_attack_option =='3':
+                self.herd_on_battlefield.dinosaurs_in_herd[0].dino_health += self.fleet_on_battlefield.robots_in_fleet[2].weapon_chosen[1]
+                self.robo_turn_over = True
+            else:
+                self.user_robo_attack_option = input("Invalid command. Try again:")
+        self.check_health()
+        self.dino_turn()
 
+    #think about maybe breaking these up into three variables so that if 1 dies it doesn't show up as an option.
     def show_dino_attack_options(self):
-        pass
-
+        print(f"*************************************************************\n-Dino's Turn-\nYour Dino Team\nEnter '1' for : {self.herd_on_battlefield.dinosaurs_in_herd[0].dino_name} ready to attack with {self.herd_on_battlefield.dinosaurs_in_herd[0].attack_chosen[0]} at {self.herd_on_battlefield.dinosaurs_in_herd[0].dino_health} heath, with {self.herd_on_battlefield.dinosaurs_in_herd[0].dino_energy_level}% energy level.\nEnter '2' for : {self.herd_on_battlefield.dinosaurs_in_herd[1].dino_name} ready to attack with {self.herd_on_battlefield.dinosaurs_in_herd[1].attack_chosen[0]} at {self.herd_on_battlefield.dinosaurs_in_herd[1].dino_health} heath, with {self.herd_on_battlefield.dinosaurs_in_herd[1].dino_energy_level}% energy level.\nEnter '3' for : {self.herd_on_battlefield.dinosaurs_in_herd[2].dino_name} ready to attack with {self.herd_on_battlefield.dinosaurs_in_herd[2].attack_chosen[0]} at {self.herd_on_battlefield.dinosaurs_in_herd[2].dino_health} heath, with {self.herd_on_battlefield.dinosaurs_in_herd[2].dino_energy_level}% energy level.\n")
+    
+    #think about maybe breaking these up into three variables so that if 1 dies it doesn't show up as an option.
     def show_robo_attack_options(self):
-        pass
+        print(f"*************************************************************\n-Robo's Turn-\nYour Robo Team\nEnter '1' for : {self.fleet_on_battlefield.robots_in_fleet[0].robo_name}, armed with a {self.fleet_on_battlefield.robots_in_fleet[0].weapon_chosen[0]}, at {self.fleet_on_battlefield.robots_in_fleet[0].robo_health} health, with {self.fleet_on_battlefield.robots_in_fleet[0].robo_energy_level}% power level.\nEnter '2' for : {self.fleet_on_battlefield.robots_in_fleet[1].robo_name}, armed with a {self.fleet_on_battlefield.robots_in_fleet[1].weapon_chosen[0]}, at {self.fleet_on_battlefield.robots_in_fleet[1].robo_health} health, with {self.fleet_on_battlefield.robots_in_fleet[1].robo_energy_level}% power level.\nEnter '3' for : {self.fleet_on_battlefield.robots_in_fleet[2].robo_name}, armed with a {self.fleet_on_battlefield.robots_in_fleet[2].weapon_chosen[0]}, at {self.fleet_on_battlefield.robots_in_fleet[2].robo_health} health, with {self.fleet_on_battlefield.robots_in_fleet[2].robo_energy_level}% power level.")
 
     def display_winners(self):
         pass
+
+    def check_health(self):
+        if self.herd_on_battlefield.dinosaurs_in_herd[0].dino_health >= 0:
+            print(f"Look at that BLOOD! {self.herd_on_battlefield.dinosaurs_in_herd[0].dino_name} definitely isn't getting up again.")
+            del self.herd_on_battlefield.dinosaurs_in_herd[0]
+        elif self.fleet_on_battlefield.robots_in_fleet[0].robo_health >= 0:
+            print(f"What a hit {self.fleet_on_battlefield.robots_in_fleet[0].robo_name} Is down for the count!")
 

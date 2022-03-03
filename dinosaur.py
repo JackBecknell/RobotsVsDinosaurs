beb19d9fc93512884fad4c573b519d8acb4695a8
JackBecknell <52moosefit@gmail.com>
2022-03-03 13:43:04 -0500

    Started laying the groundwork for the bonus points.

--- dinosaur.py
+++ dinosaur.py
@@ -3,6 +3,7 @@ class Dinosaur:
         self.dino_name = pass_name
         self.dino_attack_power = pass_attack_power
         self.dino_health = 100
+        self.dino_energry_level = 100
 
     def dino_attack(self, pass_robot_to_be_attacked):
         pass
--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ class Robot:
     def __init__(self, pass_name):
         self.robo_name = pass_name
         self.robo_health = 100
+        self.robo_energry_level = 100
         self.robo_weapon = Weapon()
     
     def robo_attack(self, pass_dinosaur_to_be_attacked):
